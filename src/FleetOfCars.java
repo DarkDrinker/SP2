4577d2e14003bcefd2348ba0369e634e6f51de72
DarkDrinker <Anders.wade.jensen@hotmail.com>
2023-04-18 13:39:30 +0200

    Initial commit

---
+++ src/FleetOfCars.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FleetOfCars {
+    private ArrayList<Car> fleet;
+
+    public FleetOfCars(){
+        fleet = new ArrayList<Car>();
+    }
+
+    public void addCar(Car car){
+    fleet.add(car);
+    }
+
+    public int getTotalRegistrationFeeForFleet(){
+        int total=0;
+        for(Car x: fleet) {
+            total += x.getRegistrationFee();
+        }
+        return total;
+    }
+
+    public String toString() {
+        for(Car x: fleet){
+            System.out.print(" "+x);
+            System.out.println("\n");
+        }
+
+        return "";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        FleetOfCars fleet = new FleetOfCars();
+        GasolineCar car1 = new GasolineCar("AK43202", "Audi","A6",4,10);
+        GasolineCar car2 = new GasolineCar("PS20981", "Mercedes","EQS",4,25);
+        DieselCar car3 = new DieselCar("KO39292", "Opel","655",4,20,true);
+        DieselCar car4 = new DieselCar("JN91753", "Honda","Civic",4,6,false);
+        ElectricCar car5 = new ElectricCar("RD25632","Tesla","y",4,70,500);
+        ElectricCar car6 = new ElectricCar("BL08621","Mustang","Mach-E",4,100,600);
+        fleet.addCar(car1);
+        fleet.addCar(car2);
+        fleet.addCar(car3);
+        fleet.addCar(car4);
+        fleet.addCar(car5);
+        fleet.addCar(car6);
+        
+        System.out.println(fleet);
+        System.out.println(fleet.getTotalRegistrationFeeForFleet());
+
+    }
+}
